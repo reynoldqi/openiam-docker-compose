7e65f394256eaf6ae2f86c7826a84d75b34482df
Lev Bornovalov <lbornov2@gmail.com>
2017-08-10 16:40:24 +0200

    fix CI and add support for connectors

--- 2.1/mysql/ci/build.sh
+++ 2.1/mysql/ci/build.sh
@@ -13,5 +13,5 @@ sudo -E docker-compose pull
 sudo -E docker-compose up -d
 
 sudo chown -R circleci /home/circleci/.docker/
-sudo /bin/bash /usr/local/bin/check_health.sh ui 15
-sudo /bin/bash /usr/local/bin/check_health.sh esb 15
+sudo /bin/bash /usr/local/bin/check_health.sh ui 30
+sudo /bin/bash /usr/local/bin/check_health.sh esb 30
--- 3.2/ci/build.sh
+++ 3.2/ci/build.sh
@@ -13,5 +13,5 @@ sudo chown -R circleci /home/circleci/.docker/
 /bin/sh setup.sh
 /bin/sh startup.sh
 
-sudo /bin/bash /usr/local/bin/check_health.sh ui 15
-sudo /bin/bash /usr/local/bin/check_health.sh esb 15
+sudo /bin/bash /usr/local/bin/check_health.sh ui 30
+sudo /bin/bash /usr/local/bin/check_health.sh esb 30
