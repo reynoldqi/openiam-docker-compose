2e8860ed368a99b8fe3f444bc7e945b681138883
Lev Bornovalov <lbornov2@gmail.com>
2017-08-13 16:08:52 +0200

    fix CI check

--- 2.1/mysql/ci/build.sh
+++ 2.1/mysql/ci/build.sh
@@ -13,11 +13,5 @@ sudo -E docker-compose pull
 sudo -E docker-compose up -d
 
 sudo chown -R circleci /home/circleci/.docker/
-sudo /bin/bash /usr/local/bin/check_health.sh ui 30
 sudo /bin/bash /usr/local/bin/check_health.sh esb 30
-sudo /bin/bash /usr/local/bin/check_health.sh idm 30
-sudo /bin/bash /usr/local/bin/check_health.sh groovy_manager 30
-sudo /bin/bash /usr/local/bin/check_health.sh workflow 30
 sudo /bin/bash /usr/local/bin/check_health.sh mariadb 30
-sudo /bin/bash /usr/local/bin/check_health.sh rabbitmq 30
-sudo /bin/bash /usr/local/bin/check_health.sh redis 30
--- 3.2/ci/build.sh
+++ 3.2/ci/build.sh
@@ -13,11 +13,5 @@ sudo chown -R circleci /home/circleci/.docker/
 /bin/sh setup.sh
 /bin/sh startup.sh
 
-sudo /bin/bash /usr/local/bin/check_health.sh ui 30
 sudo /bin/bash /usr/local/bin/check_health.sh esb 30
-sudo /bin/bash /usr/local/bin/check_health.sh idm 30
-sudo /bin/bash /usr/local/bin/check_health.sh groovy_manager 30
-sudo /bin/bash /usr/local/bin/check_health.sh workflow 30
 sudo /bin/bash /usr/local/bin/check_health.sh mariadb 30
-sudo /bin/bash /usr/local/bin/check_health.sh rabbitmq 30
-sudo /bin/bash /usr/local/bin/check_health.sh redis 30
