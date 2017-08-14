76f4b7d1927a5cc0bfb7165cd8fe33ebc8b66a90
Lev Bornovalov <lbornov2@gmail.com>
2017-08-14 09:24:07 +0200

    fix build

--- 2.1/mysql/back-end/build.sh
+++ 2.1/mysql/back-end/build.sh
@@ -8,10 +8,9 @@ sudo docker run -dit openiamdocker/circleci_build_utils:latest
 sudo docker cp $(docker ps -a -q -n 1):/opt/openiam/webapps/ /tmp/
 sudo cp -r /tmp/webapps/scripts/*.sh /usr/local/bin/
 
-cd $1
 sudo -E docker-compose pull
 sudo -E docker-compose up -d
 
-if [ -z "$2" ]; then
-    sudo /bin/bash /usr/local/bin/check_health.sh $2 15
-fi
+sudo chown -R circleci /home/circleci/.docker/
+sudo /bin/bash /usr/local/bin/check_health.sh esb 30
+sudo /bin/bash /usr/local/bin/check_health.sh mariadb 30
