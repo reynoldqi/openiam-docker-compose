0a3fa15bb8d68bcd1e2a0d462daa82aa70fc113c
Lev Bornovalov <lbornov2@gmail.com>
2017-08-16 10:14:50 +0200

    fix ldap connector

--- 3.2/setup.sh
+++ 3.2/setup.sh
@@ -5,15 +5,16 @@ set -e
 
 export VERSION=${OPENIAM_VERSION_NUMBER:-4.0.0}
 export ENVIRONMENT=${BUILD_ENVIRONMENT:-dev}
+export MARIADB_BASE_IMAGE_TYPE="debian"
 
 docker network create --attachable  --driver=overlay openiam
 
-docker pull "openiamdocker/esb:${VERSION}-${ENVIRONMENT}"
-docker pull "openiamdocker/workflow:${VERSION}-${ENVIRONMENT}"
-docker pull "openiamdocker/idm:${VERSION}-${ENVIRONMENT}"
-docker pull "openiamdocker/groovy-manager:${VERSION}-${ENVIRONMENT}"
-docker pull "openiamdocker/mariadb:${VERSION}-${ENVIRONMENT}"
-docker pull "openiamdocker/redis:${VERSION}-${ENVIRONMENT}"
-docker pull "openiamdocker/elasticsearch:${VERSION}-${ENVIRONMENT}"
-docker pull "openiamdocker/rabbitmq:${VERSION}-${ENVIRONMENT}"
-docker pull "openiamdocker/ui:${VERSION}-${ENVIRONMENT}"
+docker pull "openiamdocker/esb:alpine-${VERSION}-${ENVIRONMENT}"
+docker pull "openiamdocker/workflow:alpine-${VERSION}-${ENVIRONMENT}"
+docker pull "openiamdocker/idm:alpine-${VERSION}-${ENVIRONMENT}"
+docker pull "openiamdocker/groovy-manager:alpine-${VERSION}-${ENVIRONMENT}"
+docker pull "openiamdocker/mariadb:${MARIADB_BASE_IMAGE_TYPE}-${VERSION}-${ENVIRONMENT}"
+docker pull "openiamdocker/redis:alpine-${VERSION}-${ENVIRONMENT}"
+docker pull "openiamdocker/elasticsearch:alpine-${VERSION}-${ENVIRONMENT}"
+docker pull "openiamdocker/rabbitmq:alpine-${VERSION}-${ENVIRONMENT}"
+docker pull "openiamdocker/ui:alpine-${VERSION}-${ENVIRONMENT}"
