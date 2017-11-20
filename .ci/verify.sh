d2b96a10ca9cad5eb625ad602d5ff4e43199f6eb
Lev Bornovalov <lbornov2@gmail.com>
2017-11-20 09:19:40 +0100

    fix build

--- .ci/verify.sh
+++ .ci/verify.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
 
 set -e
-set -x
 
-sudo chown -R circleci /home/circleci/.docker/
-sudo /bin/bash /usr/local/bin/check_health.sh elasticsearch 30
-sudo /bin/bash /usr/local/bin/check_health.sh rabbitmq 30
-sudo /bin/bash /usr/local/bin/check_health.sh mariadb 30
-sudo /bin/bash /usr/local/bin/check_health.sh redis 30
+/bin/bash /usr/local/bin/check_health.sh elasticsearch 30
+/bin/bash /usr/local/bin/check_health.sh rabbitmq 30
+/bin/bash /usr/local/bin/check_health.sh mariadb 30
+/bin/bash /usr/local/bin/check_health.sh redis 30
 
-sudo /bin/bash /usr/local/bin/check_health.sh idm 30
-sudo /bin/bash /usr/local/bin/check_health.sh groovy_manager 30
-sudo /bin/bash /usr/local/bin/check_health.sh workflow 30
-sudo /bin/bash /usr/local/bin/check_health.sh esb 30
+/bin/bash /usr/local/bin/check_health.sh idm 30
+/bin/bash /usr/local/bin/check_health.sh groovy_manager 30
+/bin/bash /usr/local/bin/check_health.sh workflow 30
+/bin/bash /usr/local/bin/check_health.sh esb 30
