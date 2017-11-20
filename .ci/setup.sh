e1b5dfd84d55d11c6394140fffb7e7125b9c821e
Lev Bornovalov <lbornov2@gmail.com>
2017-11-20 09:16:18 +0100

    fix build

--- .ci/setup.sh
+++ .ci/setup.sh
@@ -3,46 +3,8 @@
 set -e
 set -x
 
-export DOCKER_COMPOSE_VERSION="1.14.0"
-
-# make user sudoer
-sudo usermod -aG sudo circleci
-
-# remove mysql
-sudo apt-get remove mysql
-
-sudo apt-get install -y curl bash git wget unzip
-
-# install docker
-# see https://docs.docker.com/engine/installation/linux/docker-ce/ubuntu/#install-using-the-repository
-sudo apt-get remove docker docker-engine docker.io
-sudo rm -rf /usr/local/bin/docker-compose*
-sudo apt-get update
-sudo apt-get install \
-    linux-image-extra-$(uname -r) \
-    linux-image-extra-virtual
-sudo apt-get update
-sudo apt-get install \
-    apt-transport-https \
-    ca-certificates \
-    curl \
-    software-properties-common
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo apt-key fingerprint 0EBFCD88
-sudo add-apt-repository \
-   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
-   $(lsb_release -cs) \
-   stable edge"
-sudo apt-get update
-sudo apt-get install docker-ce
-# curl -L "https://github.com/docker/compose/releases/download/${DOCKER_COMPOSE_VERSION}/docker-compose-$(uname -s)-$(uname -m)" > /tmp/docker-compose
-# sudo cp /tmp/docker-compose /usr/local/bin/docker-compose
-# sudo chmod a+x /usr/local/bin/docker-compose
-sudo apt-get install python-pip
-sudo pip install docker-compose
-
-sudo docker login -u ${DOCKER_USER} -p ${DOCKER_PASS}
-sudo docker pull openiamdocker/circleci_build_utils:latest
-sudo docker run -dit openiamdocker/circleci_build_utils:latest
-sudo docker cp $(docker ps -a -q -n 1):/opt/openiam/webapps/ /tmp/
-sudo cp -r /tmp/webapps/scripts/*.sh /usr/local/bin/
+docker login -u ${DOCKER_USER} -p ${DOCKER_PASS}
+docker pull openiamdocker/circleci_build_utils:latest
+docker run -dit openiamdocker/circleci_build_utils:latest
+docker cp $(docker ps -a -q -n 1):/opt/openiam/webapps/ /tmp/
+cp -r /tmp/webapps/scripts/*.sh /usr/local/bin/
